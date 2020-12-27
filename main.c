0106d04dbd1e453d462ef244c5d2df0e2370ffd6
ssingmin <ssingmin@gmail.com>
2020-12-27 15:59:57 +0900

    집에서 한 거 파싱해서 데이터 실제로 날리는 것 처럼 날리기

--- main.c
+++ main.c
@@ -9,23 +9,26 @@
 
 #define buf_size 100
 #define buf_searching 10
+#define FileSize 400	//나중에 파일사이즈를 알아내서 배열을 선언하는 식으로 바꿀 것
 
 int main(void)
 { 
 	int fd = 0;
 	int fd_1 = 0;
 	int len = 0;
-	char *temp = "hihi\n";
+	
 	char buf[buf_size] = {0,};
 
 	char *hextemp[buf_searching];
 	int hexflag = 0;
 	char filepath[30] = "hexfile/";
 	
-	char contents[200][50] = {0,};
+	char contents[FileSize][50] = {0,};
 	int Eof = buf_size;
 	int contents_line=-1, y=0;
 
+	int temp = 0;
+	unsigned char tmp_buf[21] = {0,};
 
 	printf("Start iap test\n");
 	printf("\n");
@@ -82,40 +85,32 @@ int main(void)
 				{
 					contents_line++;
 					y=0;
-					contents[contents_line][y]=buf[i];
 				}
 				else
 				{
-					y++;
 					contents[contents_line][y]=buf[i];
+					y++;
 				}
 			}
 
 			for(int i=0;i<buf_size;i++){buf[i]=0;}	//buf init
 		}while((Eof=read(fd, buf, buf_size))>0);
-
-		/* if((fd_1 = open("hexfile/writetest.txt", O_WRONLY | O_CREAT , 0666)) == -1)	//here  
+		
+		#if 0
+		if((fd_1 = open("hexfile/writetest.txt", O_WRONLY | O_CREAT , 0666)) == -1)	//here  
 		{ 
 			perror("open failed"); 
 			exit(1); 
 		} 
-		else {printf("file open success\n");}  */
-
-	/*	printf("\n-------------------------\n");
-		printf("%d", strlen(contents[1]));
-		printf(contents[1]);
-		printf("\n-------------------------\n"); */
-
-		//parsing
-		int temp = 0;
-		char tmp_buf[21] = {0,};
+		else {printf("file open success\n");}
+		#endif
+		printf("%d\n", contents_line);//	마지막 배열 이후 끝점을 알아낼 것
+		printf("%d\n", contents[190][0]);//	마지막 배열 이후 끝점을 알아낼 것
 		
-		for(int i=0;i<44;i++)
-		{
-			contents[0][i]=contents[0][i+1];
-			printf("%x\n", contents[0][i]);//
-		}
+		//parsing!!!!!!!!!!!!!!!
 
+		
+		#if 1
 		while(contents[0][temp] != 0x0A)
 		{
 			if(temp%2 == 0)	//number of ten
@@ -141,21 +136,20 @@ int main(void)
 					tmp_buf[(temp/2)] += contents[0][temp]-0x37;
 				}
 			}
-
 			temp++;
 		}
 
 			printf("\n-------------------------\n");
 			for(int i=0;i<22;i++)
 			{
-				printf("%d\n", tmp_buf[i]);//
+				printf("%1X ", tmp_buf[i]);//
 			} 
 			printf("\n-------------------------\n");
-
+		#endif
 		#if 0
 		for(int i=0;i<(contents_line+1);i++)
 		{
-			if(len=write(fd_1, contents[i], strlen(contents[i]))==-1) //아스키 파일로 되어있는 헥사 파일을 16진수로 바꿔서 다른 배열에 저장할 것
+			if(len=write(fd_1, contents[i], strlen(contents[i]))==-1)
 			{ 
 				perror("!!!!!open failed"); 
 				exit(1); 
